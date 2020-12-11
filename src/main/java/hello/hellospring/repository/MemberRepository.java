da089c91fc30e437a5d50dac6f112b0cae8d30ef
berryberry189 <58412521+berryberry189@users.noreply.github.com>
2020-12-11 22:47:05 +0900

    회원 관리 예제 -회원 도메인과 리포지토리 만들기

---
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -0,0 +1,15 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+
+    Member save(Member member);
+    Optional<Member> findById(Long Id);
+    Optional<Member> findByName(String name);
+    // Optional - 자바8에 들어가있는 기능. Wapper Class. null로 반환되는 경우에도 npe 안 떨어짐
+    List<Member> fimdAll();
+}
---
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,37 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long,Member> store = new HashMap<>();
+    private static Long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long Id) {
+        // null 이어도 Optional 감싸서 반환하면 null point exception 안 떨어짐
+        return Optional.ofNullable(store.get(Id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return  store.values().stream()
+                    .filter(member -> member.getName().equals(name))
+                    .findAny();
+
+    }
+
+    @Override
+    public List<Member> fimdAll() {
+        return new ArrayList<>(store.values());
+    }
+}
